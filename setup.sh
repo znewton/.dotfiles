174945a9355ef8d3e2abb7f78a56b8c3e2529934
Zach Newton <znewton@iastate.edu>
2018-08-28 10:14:30 -0500

    Polish setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,48 @@
 #!/usr/bin/env bash
 
+# ---------
+# ZSH Setup
+# ---------
+
+echo "Checking ZSH..."
+
+if ! test -n $ZSH_VERSION; then
+  echo "Install ZSH? (Y/n)"
+  read confirm
+  if [[ -z "$confirm" ]] || [[ "$confirm" =~ y|Y|yes ]]; then
+    if type apt-get &>/dev/null; then
+      sudo apt-get update
+      sudo apt-get upgrade
+      sudo apt-get install zsh
+    elif type brew &>/dev/null; then
+      brew install zsh
+    fi
+    sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+    zsh
+  fi
+fi
+
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  echo "Install OhMyZSH? (Y/n)"
+  read confirm
+
+  if [[ -z "$confirm" ]] || [[ "$confirm" =~ y|Y|yes ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  fi
+fi
+
 # -------------
 # Linking Files
 # -------------
 
+echo "Linking dotfiles..."
+
 files_to_link=( ".bash_profile" ".zshrc" ".vimrc" ".zsh_functions" ".zsh_greeting" ".zsh_aliases" )
 
 OLD_DOTS=".old_dotfiles"
+if [[ -d "$HOME/$OLD_DOTS" ]]; then
+  rm -rf "$HOME/$OLD_DOTS"
+fi
 mkdir "$HOME/$OLD_DOTS"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -26,9 +62,11 @@ done
 # Git Configuration
 # -----------------
 
-echo -n "GitHub Name (e.g. John Smith): "
+echo "Configuring Git..."
+
+echo -n "Git Name (e.g. John Smith): "
 read ghubName
-echo -n "GitHub Email: "
+echo -n "Git Email: "
 read ghubEmail
 
 git config --global user.name "$ghubName"
@@ -47,5 +85,11 @@ git config --global alias.visual '!gitk'
 # Vim Configuration
 # -----------------
 
+echo "Configuring Vim..."
 
+if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
 
