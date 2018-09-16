e753a891c6cd1a5093af90c2592e669aeb925951
Zach Newton <znewton@iastate.edu>
2018-09-16 11:06:19 -0500

    Add package installations to setup

--- setup.sh
+++ setup.sh
@@ -31,6 +31,15 @@ if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
   fi
 fi
 
+echo "Installing dev dependencies"
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+
+sudo apt-get install -y nodejs yarn gcc make zip unzip openjdk-8-jre openjdk-8jdk maven
+
 # -------------
 # Linking Files
 # -------------
